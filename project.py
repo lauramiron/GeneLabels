0fac9a2a6225141fd8ba4e6c91f1d3dc171f8d5a
lauramiron <laurabmiron@gmail.com>
2018-12-09 20:12:53 -0800

    building bayes predictor

--- project.py
+++ project.py
@@ -8,13 +8,19 @@ import obonet
 import subprocess
 import time
 import argparse
+from pgmpy.models import BayesianModel
+from pgmpy.factors.discrete.CPD import TabularCPD
+from numpy.random import RandomState
 
 RAND_SEED=0
 RAND_STATE=np.random.RandomState(seed=RAND_SEED)
+RAND_STATE2=np.random.RandomState(seed=RAND_SEED+1)
 DATA_DIR =r'data/'
 OUTPUT_DIR=r'output/'
 RESULTS_DIR=r'results/'
 RUNNING_MODEL_DIR = RESULTS_DIR+'1-running_model'
+MODEL_CPDS_FILE = RUNNING_MODEL_DIR+'/model_cpds.p'
+TRUE_LABEL_CPDS_FILE = RUNNING_MODEL_DIR+'/true_label_cpds.p'
 MICROARRAY_SERIES = ['GSM992', 'GSM1000', 'GSM993', 'GSM994', 'GSM995', 'GSM996', 'GSM998', 'GSM1004', 'GSM1005', 'GSM1006', 'GSM1008', 'GSM1012', 'GSM1015', 'GSM1007', 'GSM1009', 'GSM1013', 'GSM1014', 'GSM1105', 'GSM1100', 'GSM1101', 'GSM1104', 'GSM895', 'GSM1106', 'GSM1107', 'GSM1102', 'GSM1103', 'GSM1111', 'GSM899', 'GSM1041', 'GSM1047', 'GSM1042', 'GSM1043', 'GSM1044', 'GSM1045', 'GSM1046', 'GSM1055', 'GSM1029', 'GSM1030', 'GSM1032', 'GSM1033', 'GSM1034', 'GSM1048', 'GSM1049', 'GSM1050', 'GSM1051', 'GSM1052', 'GSM1053', 'GSM1054', 'GSM1075', 'GSM1076', 'GSM1090', 'GSM1077', 'GSM1078', 'GSM883', 'GSM930', 'GSM929', 'GSM928', 'GSM926', 'GSM925', 'GSM854', 'GSM855', 'GSM856', 'GSM857', 'GSM864', 'GSM865', 'GSM868', 'GSM872', 'GSM1002', 'GSM1003', 'GSM842', 'GSM843', 'GSM844', 'GSM845', 'GSM846', 'GSM847', 'GSM848', 'GSM849', 'GSM850', 'GSM851', 'GSM880', 'GSM881', 'GSM882', 'GSM874', 'GSM875', 'GSM876', 'GSM877', 'GSM878', 'GSM879', 'GSM972', 'GSM1039', 'GSM1040', 'GSM1037', 'GSM938', 'GSM939', 'GSM907', 'GSM990', 'GSM991', 'GSM997', 'GSM999', 'GSM1001', 'GSM971', 'GSM1057', 'GSM1058', 'GSM1059', 'GSM1060', 'GSM1061', 'GSM1063', 'GSM1064', 'GSM961', 'GSM962', 'GSM963', 'GSM964', 'GSM965', 'GSM966', 'GSM967', 'GSM968', 'GSM1019', 'GSM1020', 'GSM1021', 'GSM1022', 'GSM1023', 'GSM934', 'GSM935', 'GSM936', 'GSM1025', 'GSM937', 'GSM1024', 'GSM918', 'GSM919', 'GSM932', 'GSM933', 'GSM980', 'GSM863', 'GSM921', 'GSM920', 'GSM988', 'GSM922', 'GSM989', 'GSM858', 'GSM902', 'GSM931', 'GSM861', 'GSM862', 'GSM923', 'GSM860', 'GSM924', 'GSM859', 'GSM940', 'GSM942', 'GSM910', 'GSM969', 'GSM970', 'GSM973', 'GSM974', 'GSM975', 'GSM976', 'GSM984', 'GSM977', 'GSM903', 'GSM906', 'GSM985']
 GO_NODES_LIST_FILE = DATA_DIR+'go_nodes_list.txt'
 GO_LIST_FORMATTED = OUTPUT_DIR+'go_nodes_formatted.txt'
@@ -86,18 +92,6 @@ def GDSFiles_to_Dict():
 	print("Discarding "+str(num_genes-len(filtered_dict.keys()))+" genes with missing features, leaving "+str(len(filtered_dict.keys()))+" genes with full MA expression data")
 	pickle.dump(filtered_dict, open(MA_RAW_DICT_FILE, "wb"))
 
-
-# # Discard microarray genes where entire experiments / feature sets are missing
-# # N.B. This does not discard genes where experiment results were recorded as null
-# def DiscardMAGenesWithMissingFeatures():
-# 	data_dict = pickle.load(open('microarray_dict.p','rb'))
-# 	ma_genes_list = []
-# 	with open('microarray_genes_list.txt','w') as f:
-# 		for gene in data_dict.keys():
-# 			if set(list(data_dict[gene].keys())) == set(MICROARRAY_SERIES):
-# 				ma_genes_list.append(gene)
-# 				f.write(gene+'\n')
-# 	return ma_genes_list
 
 def ConstructMicroarrayArray(example_key='851262'):
 	data_dict = pickle.load(open(MA_RAW_DICT_FILE,'rb'))
@@ -182,6 +176,7 @@ def RunKnnOnNulls():
 	assert(np.isnan(data_arr).any()==False)
 	np.savetxt(MA_NONNULL_DATA,data_arr)
 
+
 def MakeGeneAndGoDicts():
 	# make dict of index-to-GOTerm, using set 105 terms
 	go_dict = {}
@@ -205,6 +200,7 @@ def MakeGeneAndGoDicts():
 	pickle.dump(genes_dict,open(GENES_DICT_FILE,'wb'))
 	print("MA_genes: "+str(len(MA_genes))+", GO_genes:"+str(len(GO_genes))+", PAIR_genes:"+str(len(PAIR_genes)))
 	print("Found "+str(len(genes_dict.keys()))+" intersecting genes across all sets")
+
 
 def FixOutOfDataGoIds():
 	# reformat hardcoded list of curated GO ids
@@ -302,6 +298,7 @@ def ConstructGoAnnotationArray():
 	np.savetxt(GO_LABEL_ARR_FILE,go_labels)
 	print("Constructed go annotation array with "+str(m)+" examples and "+str(f)+" annotations")
 
+
 def ConstructPairwiseArray():
 	full_pairwise_data = np.loadtxt(PAIR_FULL_DATA_FILE)
 	full_pairwise_dict = pickle.load(open(PAIR_FULL_DICT_FILE,'rb'))
@@ -314,11 +311,13 @@ def ConstructPairwiseArray():
 		pairwise_data[idx] = full_pairwise_data[full_pairwise_dict[geneid]]
 	np.savetxt(PAIR_DATA_FILE,pairwise_data)
 
+
 def GetKMaxLabels(k=1):
 	# go_dict = pickle.load(open(GO_DICT_FILE,'rb'))
 	go_data = np.load(GO_LABEL_ARR_FILE)
 	# inv_go_dict = {v: k for k, v in go_dict.items()}
 	return -(go_data.sum(axis=0)).argsort()[:k]
+
 
 def LoadCombinedData():
 	pairwise_data = np.loadtxt(PAIR_DATA_FILE)
@@ -329,6 +328,13 @@ def LoadCombinedData():
 	go_dict = pickle.load(open(GO_DICT_FILE,'rb'))
 	genes_dict = pickle.load(open(GENES_DICT_FILE,'rb'))
 	return data, label_data, {v: k for k, v in go_dict.items()}, {v: k for k, v in genes_dict.items()}
+
+def load_label_data():
+	label_data = np.loadtxt(GO_LABEL_ARR_FILE)
+	assert(label_data.shape[0]==label_data.shape[0])
+	go_dict = pickle.load(open(GO_DICT_FILE,'rb'))
+	genes_dict = pickle.load(open(GENES_DICT_FILE,'rb'))
+	return label_data, go_dict, {v: k for k, v in go_dict.items()}, {v: k for k, v in genes_dict.items()}
 
 # def _modelfnames(model_name='defaultparams'):
 # 	timestring = time.strftime("%m-%d-%H.%M")
@@ -345,6 +351,7 @@ def _timeModelDir(model_name='lineardefault'):
 	dirname = RESULTS_DIR+model_name+'_'+timestring
 	os.mkdir(dirname)
 	return dirname
+
 
 def DefaultParametersFullData(kernel='linear',C=1.0,gamma='auto',n_estimators=1,resume=True,startID=0):
 	print('-----Running SVM on all GO IDs-----')   
@@ -375,15 +382,16 @@ def DefaultParametersFullData(kernel='linear',C=1.0,gamma='auto',n_estimators=1,
 	os.rename(model_dir,_timeModelDir(model_name='lineardefault'))
 
 
-def make_test_set(training_examples,training_labels):
+def make_test_set(training_examples,training_labels,rstate=RAND_STATE):
 	num_test_samples = int(training_examples.shape[0]*0.632)
-	test_idxs = RAND_STATE.choice(range(0,training_examples.shape[0]),size=num_test_samples,replace=True)
+	test_idxs = rstate.choice(range(0,training_examples.shape[0]),size=num_test_samples,replace=True)
 	test_set = np.zeros(shape=(num_test_samples,training_examples.shape[1]))
-	test_labels = np.zeros(shape=(num_test_samples,1))
+	test_labels = np.zeros(shape=(num_test_samples,training_labels.shape[1]))
 	for i in range(0,num_test_samples):
 		test_set[i] = training_examples[test_idxs[i]]
 		test_labels[i] = training_labels[test_idxs[i]]
 	return test_set, test_labels
+
 
 def test_svm_model(kernel, training_examples, training_labels, C=1.0, gamma='auto',n_estimators=10):
 	model = ensemble.BaggingClassifier(svm.SVC(kernel=kernel, gamma=gamma, random_state=RAND_SEED), n_estimators=n_estimators,max_samples=0.632)
@@ -415,20 +423,142 @@ def get_true_false_positive_negative(y_pred, y):
 	print('TP, TN, FP, FN: ', TP, TN, FP, FN,'precision:', precision, 'recall', recall)
 	return TP, TN, FP, FN, precision, recall
 
-# def test_svm_model(kernel, training_examples, training_labels, dev_set, dev_labels, test_set, test_labels, gamma='auto'):
-#     print('Kernel:', kernel, ',gamma:', gamma)
-#     model = ensemble.BaggingClassifier(svm.SVC(kernel=kernel, gamma=gamma, random_state=0), max_samples=0.632)
-#     model.fit(training_examples, training_labels)
-#     print('dev score: ', model.score(dev_set, dev_labels))
-#     print('test score: ', model.score(test_set, test_labels))
-#     true_false_positive_negative = get_true_false_positive_negative(model.predict(dev_set), dev_labels)
-#     print('TP, TN, FP, FN, dev: ', true_false_positive_negative[:4],
-#           'precision:', true_false_positive_negative[4], 'recall', true_false_positive_negative[5])
-#     true_false_positive_negative = get_true_false_positive_negative(model.predict(test_set), test_labels)
-#     print('TP, TN, FP, FN, test: ', true_false_positive_negative[:4],
-#           'precision:', true_false_positive_negative[4], 'recall', true_false_positive_negative[5])
-#     return model
+def _dumps(models_dict,file_path):
+	n_bytes = 2**31
+	max_bytes = n_bytes - 1
+	## write
+	bytes_out = pickle.dumps(models_dict)
+	with open(file_path, 'wb') as f_out:
+		for idx in range(0, len(bytes_out), max_bytes):
+			f_out.write(bytes_out[idx:idx+max_bytes])	
 
+def _loads(file_path):
+	n_bytes = 2**31
+	max_bytes = n_bytes - 1
+	## read
+	bytes_in = bytearray(0)
+	input_size = os.path.getsize(file_path)
+	with open(file_path, 'rb') as f_in:
+		for _ in range(0, input_size, max_bytes):
+			bytes_in += f_in.read(max_bytes)
+	data2 = pickle.loads(bytes_in)
+	return data2
+
+
+def _load_models_dict():
+	model_dir = RUNNING_MODEL_DIR
+	models_file = model_dir+'/all_models.p'
+	if not os.path.isfile(models_file):
+		print('Loading models from individual files')
+		models_dict = {}
+		for model_file in glob.glob(model_dir+'/model-*.p'):
+			name = 'GO:'+model_file.split('/')[-1].split('-')[-1].split('.')[0]
+			model = pickle.load(open(model_file,'rb'))
+			models_dict[name] = model
+		_dumps(models_dict,models_file)
+		# pickle.dump(models_dict,open(models_file,'wb'))
+	else:
+		print('Loading models from single file')
+		# models_dict = pickle.load(open(models_file,'rb'))
+		models_dict = _loads(models_file)
+	return models_dict
+
+def get_model_cpds(training_data,training_labels_negs):
+	print('Getting cpds from svm predictions on test set')
+	# training_data, training_labels, go_inv_dict, genes_inv_dict = LoadCombinedData()
+	training_labels = np.copy(training_labels_negs)
+	training_labels[np.where(training_labels==-1)] = 0
+	assert((training_labels!=-1).any())
+	validation_data, validation_labels = make_test_set(training_data,training_labels,rstate=RAND_STATE2)
+	models_dict = _load_models_dict()
+	cpds = []
+	i=1
+	num_models = len(models_dict.keys())
+	for model_name in models_dict:
+		print(i,'/',num_models,' ',model_name)
+		i+=1
+		model = models_dict[model_name]
+		y_pred = model.predict(validation_data)
+		yhat0_y0 = (1-y_pred[validation_labels==0]).sum() / (1-validation_labels).sum()
+		yhat0_y1 = (1-y_pred[validation_labels==1]).sum() / validation_labels.sum()
+		yhat1_y0 = (y_pred[validation_labels==0]).sum() / (1-validation_labels).sum()
+		yhat1_y1 = (y_pred[validation_labels==1]).sum() / validation_labels.sum()
+		cpd = TabularCPD(model+'_hat',2,[[yhat0_y0,yhat0_y1],[yhat1_y0,yhat1_y1]],evidence=[model_name],evidence_card=[2])
+		cpd.normalize()
+		cpds.append(cpd)
+	pickle.dump(cpds,open(MODEL_CPDS_FILE,'wb'))
+	return cpds
+
+
+def get_true_label_cpds(training_labels_negs,inv_go_dict):
+	print('Calculating cpds for true label dependencies')
+	training_labels = np.copy(training_labels_negs)
+	training_labels[np.where(x==-1)] = 0	
+	labels_list = inv_go_dict.values()
+	cpds = []
+	for label in labels_list:
+		children = [c for c in networkx.ancestors(obo_graph,label) if c in labels_list]
+		data = np.zeros(shape=(2,2**len(children)))
+		data[1,:] = 1
+		goidx = inv_go_dict[label]
+		tlc = np.copy(training_labels)
+		cgidxs = [inv_go_dict[c] for c in children]
+		for gid in cgids:
+			np.delete(tlc,np.where(tlc[:,gid]==1))
+		data[1,0] = tlc.sum(axis=0)[goidx] / tlc.shape[0]
+		data[0,0] = 1 - data[1,0]
+
+		evidence = []
+		evidence_card = []
+		for child in children:
+			evidence.append(child)
+			evidence_card.append(2)
+		cpd = TabularCPD(label,2,data,evidence=evidence,evidence_card=evidence_card)
+		cpds.append(cpd)
+	pickle.dump(cpds,open(TRUE_LABEL_CPDS_FILE,'wb'))
+	return cpds
+
+
+def make_bayes_net():
+	print('Making bayes net')
+	print('loading data...')
+	training_data, training_labels, inv_go_dict, inv_genes_dict = LoadCombinedData()
+	go_dict = {v: k for k, v in inv_go_dict.items()}
+	labels_list = go_dict.keys()
+
+	print('adding nodes and edges...')
+	G = BayesianModel()
+	G.add_nodes_from(labels_list)
+	G.add_nodes_from([label+'_hat' for label in labels_list])
+	G.add_edges_from([(label, label+'_hat') for label in labels_list])
+	obo_graph = obonet.read_obo(OBODB_FILE)
+	for label in labels_list:
+		children = networkx.ancestors(obo_graph,label)
+		for child in children:
+			if child in labels_list:
+				G.add_edge(child,label)
+				# cpd = TabularCPD(label,2,[[1,0],[0,1]],evidence=[child],evidence_card=[2])
+				# G.add_cpds(cpd)
+	
+	predicted_cpds = get_model_cpds(training_data,training_labels)
+	for cpd in predicted_cpds:
+		G.add_cpds(cpd)
+	pdb.set_trace()
+	true_label_cpds = get_true_label_cpds(training_labels,inv_go_dict)
+	for cpd in true_label_cpds:
+		G.add_cpds(cpd)
+	pdb.set_trace()
+	return G
+
+	
+def BayesNetPredict():
+	training_data, training_labels, go_inv_dict, genes_inv_dict = LoadCombinedData()
+	model = make_bayes_net()
+	test_data, test_labels = make_test_set(training_data,training_labels,rstate=RandomState(seed=RAND_SEED+2))
+	columns = [go_inv_dict[i]+'_hat' for i in range(len(go_inv_dict.keys()))]
+	predict_data = pd.DataFrame(test_labels[0],columns=columns)
+	y_pred = model.predict(predict_data)
+	pdb.set_trace()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--bags',default=1,type=int)
@@ -449,7 +579,9 @@ elif action == 'ma_makearr':
 elif action == 'pair_makearr':
 	ConstructPairwiseArray()
 elif action == 'runsvm':
-	DefaultParametersFullData(n_estimators=options.bags,startID=options.startID,resume=(~options.restart))
+	DefaultParametersFullData(n_estimators=options.bags,startID=options.startID,resume=(not options.restart))
+elif action == 'runbayes':
+	BayesNetPredict()
 else: print("missing required arguments")
 
 
